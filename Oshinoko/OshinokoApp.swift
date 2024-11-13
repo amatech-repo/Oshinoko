4eef4486d0d461fcf1d0e90aa0aa77fd86acaa4d
irukahaoruka <erika03301109@gmail.com>
2024-11-13 21:57:38 +0900

    ファイル追加

---
+++ Oshinoko/OshinokoApp.swift
@@ -0,0 +1,17 @@
+//
+//  OshinokoApp.swift
+//  Oshinoko
+//
+//  Created by 櫻井絵理香 on 2024/11/13.
+//
+
+import SwiftUI
+
+@main
+struct OshinokoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
