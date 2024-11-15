589bda8fe24e70b5b1d4a01e3bbf82d12061e437
irukahaoruka <erika03301109@gmail.com>
2024-11-15 13:35:24 +0900

    feat: モデルを用意

--- Oshinoko/Models/MapModel.swift
+++ Oshinoko/Models/MapModel.swift
@@ -6,9 +6,15 @@ import MapKit
 
 // 注釈用のデータモデル
 struct MapAnnotationItem: Identifiable {
-    let id = UUID()
+    let id: UUID
     let coordinate: CLLocationCoordinate2D
     let color: Color
+
+    init(id: UUID = UUID(), coordinate: CLLocationCoordinate2D, color: Color) {
+        self.id = id
+        self.coordinate = coordinate
+        self.color = color
+    }
 }
 
 // ルート情報を格納するモデル
