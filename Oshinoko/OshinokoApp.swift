3a2d7fde31e5390e1d21c6e2891096daa051139c
irukahaoruka <erika03301109@gmail.com>
2024-11-24 15:09:20 +0900

    アプリが起動するまでのローディング画面を追加

--- Oshinoko/OshinokoApp.swift
+++ Oshinoko/OshinokoApp.swift
@@ -24,12 +24,19 @@ struct OshinokoApp: App {
     @StateObject private var appState = AppState()
     @StateObject private var authViewModel = AuthViewModel() // クラス名を統一
     @StateObject private var pinsViewModel = PinsViewModel(authViewModel: AuthViewModel.shared) // 正しい型を渡す
-    
+
     var body: some Scene {
         WindowGroup {
-            ContentView(pinsViewModel: pinsViewModel)
-                .environmentObject(appState)
-                .environmentObject(authViewModel) // 必要に応じて渡す
+            if appState.isLoading {
+                LoadingView()
+                    .onAppear {
+                        appState.finishLoading() // ローディングを開始
+                    }
+            } else {
+                ContentView(pinsViewModel: pinsViewModel)
+                    .environmentObject(appState)
+                    .environmentObject(authViewModel)
+            }
         }
     }
 }
