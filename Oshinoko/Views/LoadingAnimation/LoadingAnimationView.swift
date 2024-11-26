7936486620d915381249043b56f67172dbd70ff5
irukahaoruka <erika03301109@gmail.com>
2024-11-26 09:40:00 +0900

    add: lottieファイルを追加した

---
+++ Oshinoko/Views/LoadingAnimation/LoadingAnimationView.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingAnimationView.swift
+//  Oshinoko
+//
+//  Created by 櫻井絵理香 on 2024/11/26.
+//
+
+import SwiftUI
+
+struct LoadingAnimationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingAnimationView()
+}
